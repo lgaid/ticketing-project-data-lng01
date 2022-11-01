428b2cc259526fcecc5f8c6ecf7ef9934fe7ca2d
lgaid <104643045+lgaid@users.noreply.github.com>
2022-11-01 11:25:41 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/ticketingprojectdatalng01/TicketingProjectDataLng01Application.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectdatalng01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectDataLng01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectDataLng01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/ticketingprojectdatalng01/TicketingProjectDataLng01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.ticketingprojectdatalng01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingProjectDataLng01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
